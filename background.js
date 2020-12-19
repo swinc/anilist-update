61b3791bc85896ee6834fb14545073a286b2402d
Iain Cunningham <icunningham@samwise.tech>
2020-12-19 14:59:53 -0800

    Better org of background.js

--- background.js
+++ background.js
@@ -2,18 +2,23 @@
 
 /* globals chrome */
 
-// criteria for displaying browser icon
-var targetPage = {
+var targetSites = ['anilist.co', 'netflix.com', 'hulu.com']
+
+// rule for when to display the extention's page action
+var targetPagesRule = {
   conditions: [
     new chrome.declarativeContent.PageStateMatcher({
-      pageUrl: { hostContains: 'anilist.co' }
+      pageUrl: { hostContains: targetSites }
     })
   ],
   actions: [new chrome.declarativeContent.ShowPageAction()]
 }
 
+/* add a listener for the onPageChanged event and when the page is changed, use the
+ * "targetPage" rule above
+*/
 chrome.runtime.onInstalled.addListener(function () {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([targetPage])
+    chrome.declarativeContent.onPageChanged.addRules([targetPagesRule])
   })
 })
