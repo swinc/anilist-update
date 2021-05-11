9a3fc631e4394e696047856f0b9b50bdec3362e5
Iain Cunningham <iaincunningham@me.com>
2021-05-10 22:15:30 -0700

    Refactor

--- popup.js
+++ popup.js
@@ -1,13 +1,17 @@
 /* globals alert */
 
-import { showLoginStatus } from './lib/show-login-status.js'
+import { showConnectedAccount, showDisconnectedAccount, showUserName } from './lib/popup-ui.js'
 import { loginToAnilist } from './lib/login-to-anilist.js'
 import { clickLoginButton, clickLogoutButton } from './lib/button-handlers.js'
 import { updateUserName } from './lib/username.js'
 
 document.addEventListener('DOMContentLoaded', function() {
-  showLoginStatus()
-  updateUserName()
+  // account linkage
+  chrome.storage.sync.get(['accessToken', 'userName'], function (storage) {
+    if (storage.accessToken) { showConnectedAccount() }
+    else { showDisconnectedAccount() }
+    showUserName(storage.userName)
+  })
 
   // button handlers
   document.querySelector('#anilist-login-button').onclick = clickLoginButton
