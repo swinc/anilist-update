7399bbc8d681286fe70df0f1770f126bd53e6518
Iain Cunningham <iaincunningham@me.com>
2021-05-16 15:58:28 -0700

    Super ugly but working search bar

--- content-script.js
+++ content-script.js
@@ -2,14 +2,9 @@
 const generatedSource = new XMLSerializer().serializeToString(document);
 
 // get episode title
-const reMediaTitle = /"mediaTitle":"(.+?)"/  // https://regex101.com/r/zOb55Y/1
+const reMediaTitle = /<title>(.+?) Episode/  // https://regex101.com/r/zOb55Y/1
 let matchesMediaTitle = generatedSource.match(reMediaTitle)
 const contentTitle = matchesMediaTitle[1]
 
-// get episode number
-const reEpisodeNumber = /"episodeNumber":"(\d+?)"/
-let matchesEpisodeNumber = generatedSource.match(reEpisodeNumber)
-const episodeNumber = matchesEpisodeNumber[1]
-
 // report to extension
-chrome.runtime.sendMessage({ detected: true, title: contentTitle, episode: episodeNumber});
+chrome.runtime.sendMessage({ detected: true, title: contentTitle});
--- popup.js
+++ popup.js
@@ -8,9 +8,10 @@ import {
   showContentNotDetected,
   showMediaTitle,
   showMediaCoverImage,
-  showMediaEpisodes,
   showUserProgress,
-  showUserScore
+  showUserScore,
+  updateTitleLink,
+  prepopSearchBox
 } from './lib/popup-ui.js'
 import { loginToAnilist } from './lib/login-to-anilist.js'
 import { clickLoginButton, clickLogoutButton } from './lib/button-handlers.js'
@@ -20,6 +21,26 @@ document.addEventListener('DOMContentLoaded', function() {
   // button handlers
   document.querySelector('#anilist-login-button').onclick = clickLoginButton
   document.querySelector('#logout-link').onclick = clickLogoutButton
+
+  // querybox
+  // onReturn reexecute querySearchMedia with the search box
+  document.querySelector('#search-box').onkeydown = function() {
+    if(event.key === 'Enter') {
+      querySearchMedia(document.querySelector('#search-box').value)
+        .then((result) => {
+          showMediaCoverImage(result.data.Media.coverImage.medium)
+          showMediaTitle(result.data.Media.title.english)
+          updateTitleLink(result.data.Media.id)
+          chrome.storage.sync.get(['accessToken', 'userName'], function (storage) {
+            queryUserMediaNotes(result.data.Media.id, storage.userName)
+              .then((result) => {
+                showUserProgress(result.data.MediaList.progress)
+                showUserScore(result.data.MediaList.score)
+              })
+            })
+        })
+    }
+  }
 
   // show page info
   chrome.storage.sync.get(['accessToken', 'userName'], function (storage) {
@@ -32,11 +53,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
       if(window.contentScriptMessage && window.contentScriptMessage.detected) {
         showContentDetected(window.contentScriptMessage.title, window.contentScriptMessage.episode)
+        prepopSearchBox(window.contentScriptMessage.title)
         querySearchMedia(window.contentScriptMessage.title)
           .then((result) => {
             showMediaCoverImage(result.data.Media.coverImage.medium)
             showMediaTitle(result.data.Media.title.english)
-            showMediaEpisodes(result.data.Media.episodes)
+            updateTitleLink(result.data.Media.id)
             queryUserMediaNotes(result.data.Media.id, storage.userName)
               .then((result) => {
                 showUserProgress(result.data.MediaList.progress)
