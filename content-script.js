baa26bb99b843cdedb33095bec40ef6e44cfea9c
Iain Cunningham <iaincunningham@me.com>
2021-05-25 18:18:43 -0700

    Lint content-script.js

--- content-script.js
+++ content-script.js
@@ -1,10 +1,12 @@
+/* globals chrome, XMLSerializer */
+
 // access page source
-const generatedSource = new XMLSerializer().serializeToString(document);
+const generatedSource = new XMLSerializer().serializeToString(document)
 
 // get episode title
-const reMediaTitle = /<title>(.+?) Episode/  // https://regex101.com/r/zOb55Y/1
-let matchesMediaTitle = generatedSource.match(reMediaTitle)
+const reMediaTitle = /<title>(.+?) Episode/ // https://regex101.com/r/zOb55Y/1
+const matchesMediaTitle = generatedSource.match(reMediaTitle)
 const contentTitle = matchesMediaTitle[1]
 
 // report to extension
-chrome.runtime.sendMessage({ detected: true, title: contentTitle});
+chrome.runtime.sendMessage({ detected: true, title: contentTitle })
