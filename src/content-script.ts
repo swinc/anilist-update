624a5a35c4e5811fa86e5cf9012daeafe5773af4
sethgutierrez <sethg361@gmail.com>
2021-07-20 21:24:59 -0700

    Added functionality for beta.crunchyroll.com

--- src/content-script.ts
+++ src/content-script.ts
@@ -3,8 +3,10 @@ function getMediaTitle() {
 
   if (hostName == 'www.hulu.com') {
     contentTitle = document.querySelector('div.PlayerMetadata__title > div > div > span').textContent
-  } else if (hostName == 'www.crunchyroll.com' || hostName == 'beta.crunchyroll.com') {
+  } else if (hostName == 'www.crunchyroll.com') {
     contentTitle = document.querySelector('div.showmedia-header > h1 > a > span').textContent
+  } else if (hostName == 'beta.crunchyroll.com') {
+    contentTitle = document.querySelector('h4.c-text.c-text--l.c-text--semibold.c-text--fixed-size').textContent
   } else if (hostName == 'www.netflix.com') {
     contentTitle = document.querySelector('div.video-title > div > h4').textContent
   }
