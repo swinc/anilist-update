619b24b2706636c6ee43a58a56c295a4565e4a02
Iain Cunningham <iaincunningham@me.com>
2021-07-21 10:42:47 -0700

    Ignore build files; copy over with copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
   entry: {
@@ -23,5 +24,17 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname)
-  }
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "src/manifest.json", to: "app/manifest.json" },
+        { from: "src/static/images", to: "app/images" },
+        { from: "src/options/options.css", to: "app/options/options.css" },
+        { from: "src/options/options.html", to: "app/options/options.html" },
+        { from: "src/popup/popup.css", to: "app/popup/popup.css" },
+        { from: "src/popup/popup.html", to: "app/popup/popup.html" },
+      ],
+    }),
+  ]
 };
